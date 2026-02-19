b307470f2c947d778d7fac3b1e5af4edfe7cf191
Pranav Raj Lodhi <rajlodhipranav@gmail.com>
2026-02-20 04:19:00 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import { Activity } from 'lucide-react';
 
 function App() {
   useEffect(() => {
-    fetch("https://teamtechsapians.onrender.com")
+    fetch("https://teamtechsapians.onrender.com/api/test")
       .then(res => res.json())
       .then(data => {
         console.log("Backend Response:", data);
