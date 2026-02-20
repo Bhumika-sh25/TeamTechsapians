b11570e3c84b942fbbafc7440739cbfd253b410d
Tiwari2007 <126658857+Tiwari2007@users.noreply.github.com>
2026-02-20 06:51:44 +0530

    Change API URL for user fetch to login endpoint

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import { Activity } from 'lucide-react';
 
 function App() {
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/api/users`)
+   fetch("https://teamtechsapians.onrender.com/api/login")
       .then(res => res.json())
       .then(data => {
         console.log("Backend Response:", data);
